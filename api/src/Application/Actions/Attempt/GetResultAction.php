89373eacfcb91831805d7c0e71c74bac93207f10
Timur Gogolev <timur.gglv@gmail.com>
2022-03-21 23:40:19 +0300

    Implement getting results from API

--- api/src/Application/Actions/Attempt/GetResultAction.php
+++ api/src/Application/Actions/Attempt/GetResultAction.php
@@ -5,11 +5,35 @@ declare(strict_types=1);
 namespace App\Application\Actions\Attempt;
 
 use Psr\Http\Message\ResponseInterface as Response;
-use App\Application\Actions\Action;
+use Psr\Log\LoggerInterface;
 use Slim\Routing\RouteContext;
+
+use App\Application\Actions\Action;
+use App\Domain\Attempt\AttemptRepositoryInterface;
+use App\Domain\Topic\TopicRepositoryInterface;
 
 class GetResultAction extends Action
 {
+    /**
+     * @var AttemptRepositoryInterface
+     */
+    private $attemptRepository;
+
+    /**
+     * @var TopicRepositoryInterface
+     */
+    private $topicRepository;
+
+    public function __construct(
+        LoggerInterface $logger,
+        AttemptRepositoryInterface $attemptRepository,
+        TopicRepositoryInterface $topicRepository
+    ) {
+        parent::__construct( $logger );
+        $this->attemptRepository  = $attemptRepository;
+        $this->topicRepository    = $topicRepository;
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -17,41 +41,41 @@ class GetResultAction extends Action
     {
         $routeContext = RouteContext::fromRequest($this->request);
         $route        = $routeContext->getRoute();
-        $id           = $route->getArgument('id');
+        $attemptId    = $route->getArgument('id');
+
+        $attempt = $this->attemptRepository->fetchById( (int) $attemptId );
+        if ( false === $attempt ) {
+            return $this->respondWithData( [ 'error' => 'An attempt not found' ], 404 );
+        }
+
+        $answers = $this->attemptRepository->getAttemptAnswers( $attempt );
+        if ( ! count( $answers ) ) {
+            return $this->respondWithData( [ 'error' => 'Answers not found' ], 404 );
+        }
+
+        $topic = $this->topicRepository->fetchById( $attempt->getTopicId() );
+        if ( false === $attempt ) {
+            return $this->respondWithData( [ 'error' => 'A topic not found' ], 404 );
+        }
 
         // TODO: get results
         $data = [
-            'topic' => [
-                'id' => 1,
-                'name' => '100 verbs for beginners',
-                'approach' => 'back-and-forth',
+            'topic'   => [
+                'id'     => $topic->getId(),
+                'name'   => $topic->getName(),
+                'method' => $topic->getMethod(),
             ],
-            'answers' => [
-                [
-                    'id' => 1,
-                    'q' => 'abrir',
-                    'a' => 'to open',
-                    'q2a' => 'open',
-                    'a2q' => 'arir',
-                    'status' => 'incorrect',
-                ],
-                [
-                    'id' => 2,
-                    'q' => 'abrir',
-                    'a' => 'to open',
-                    'q2a' => 'open',
-                    'a2q' => 'arir',
-                    'status' => 'incorrect',
-                ],
-                [
-                    'id' => 3,
-                    'q' => 'acaitar',
-                    'a' => 'to accept',
-                    'q2a' => 'to accept',
-                    'a2q' => 'aceitar',
-                    'status' => 'correct',
-                ],
-            ],
+            'answers' => array_map( function( $answer ) {
+                $answers = unserialize( $answer->getAnswer() );
+                return [
+                    'id'     => $answer->getQuestionId(),
+                    'q'      => $answer->getQuestion(),
+                    'a'      => $answer->getCorrectAnswer(),
+                    'q2a'    => $answers['q2a'] ?: '---',
+                    'a2q'    => $answers['a2q'] ?: '---',
+                    'status' => $answer->isCorrect() ? 'correct' : 'incorrect',
+                ];
+            }, $answers )
         ];
 
         return $this->respondWithData( $data, 200 );
